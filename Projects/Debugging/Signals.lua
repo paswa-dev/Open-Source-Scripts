d430f9d813e70cba2bcd4ce092a32efbe5a20156
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2023-06-11 10:57:33 -0400

    Added API Documentation

--- Projects/Debugging/Signals.lua
+++ Projects/Debugging/Signals.lua
@@ -2,9 +2,27 @@ local Signal = {}
 local SignalInterpolation = require(script:WaitForChild("SignalInterpolation"))
 local HTTP, RS = game:GetService("HttpService"), game:GetService("RunService")
 
+--[[
+@API
+.iSignal(x : any) -> boolean
+.ApplyToInstance(instance: Instance, signal_properties: {[string]: Signal}, toString: boolean) -> None
+.new(value : any) -> Signal
+
+@Signal
+.to: (new: any, t: number?) -> Signal,
+.AddInstance: (instance : Instance, signal_properties: {any} | {[string]: Signal}, toString: boolean) -> nil,
+.UpdateInstances: () -> nil,
+.AutoBindInstances: (disable: boolean) -> nil
+
+
+]]
+
 type Signal = {
 	value: any,
 	to: (new: any, t: number?) -> Signal,
+	AddInstance: (instance : Instance, signal_properties: {any} | {[string]: Signal}, toString: boolean) -> nil,
+	UpdateInstances: () -> nil,
+	AutoBindInstances: (disable: boolean) -> nil
 }
 
 local function FireSignal(v, callback)
@@ -24,7 +42,7 @@ function Signal.isSignal(x)
 	return x["RenderID"] ~= nil
 end
 
-function Signal.ApplyToInstance(instance, signal_properties, toString: boolean)
+function Signal.ApplyToInstance(instance : Instance, signal_properties : {any}, toString: boolean)
 	applySignalsToInstance(instance, signal_properties, toString and tostring or nil)
 end
 
@@ -39,18 +57,18 @@ function Signal.new(value)
 		end
 		return ref_table
 	end
-	
+
 	function ref_table.AddInstance(instance : Instance, signal_properties, toString: boolean)
 		ref_table.Instances[instance] = {array=signal_properties, callback=toString and tostring or nil}
 	end
-	
+
 	function ref_table.UpdateInstances()
 		for Inst, SigArray in pairs(ref_table.Instances) do
 			applySignalsToInstance(Inst, SigArray.array, SigArray.callback)
 		end
 	end
-	
-	function ref_table.AutoBindInstances(disable)
+
+	function ref_table.AutoBindInstances(disable : boolean)
 		if disable then
 			if ref_table.isRenderActive then
 				RS:UnbindFromRenderStep(ref_table.RenderID)
@@ -63,7 +81,7 @@ function Signal.new(value)
 			end
 		end
 	end
-	
+
 	return setmetatable(ref_table, {
 		__call = function(_, value) 
 			if not value then return ref_table.Value end
