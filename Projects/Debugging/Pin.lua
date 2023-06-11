e714d069d61f64d453b869dfa0b173b3fcc3d58d
paswa-dev <73659592+paswa-dev@users.noreply.github.com>
2023-06-11 09:45:31 -0400

    Update Pin.lua

--- Projects/Debugging/Pin.lua
+++ Projects/Debugging/Pin.lua
@@ -1,73 +1,153 @@
+
 local pin = {}
+pin.Signal = require(script:WaitForChild("Signals"))
+
+pin.Init = false
+pin.DebugRoot = nil
 
 type PinData = {
-    Children: {any}
+	Children: {any}
 }
 
 type PinInfo = {
-    Name : string,
-    Adornee: Instance | Vector3,
-    PinData : PinData,
-    [any]: any,
+	Name : string,
+	Adornee: Part | Vector3,
+	PinData : PinData,
+	[any]: any,
 }
 
+---- Mapping
+
+---- UI Public Functions
+
+function pin.Make(name)
+	local UI = Instance.new(name)
+	return function(properties, parent)
+		for i, v in pairs(properties) do
+			if typeof(i) == "function" then
+				i(v, UI)
+			else
+				if not pin.Signal.isSignal(v) then
+					UI[i] = v
+				end
+			end
+		end
+		pin.Signal.ApplyToInstance(UI, properties, true)
+		if parent then UI["Parent"] = parent; properties["Parent"] = parent end
+		return UI, properties
+	end
+end
+---- Local Functions
+
 local function UpdatePinPosition(pin: PinInfo)
-    -- Do stuff
+	if typeof(pin.Adornee) == "Vector3" then
+		return workspace.CurrentCamera:WorldToScreenPoint(pin.Adornee)
+	end
+	return workspace.CurrentCamera:WorldToScreenPoint(pin.Adornee["Position"] or pin.Adornee:GetPivot())
 end
 
 local function MakePinData(...): PinData
-    local data = {
-        Children = {}
-    }
-    for _, v in next, {...} do table.insert(data.Children, v) end
-    return data
+	local data = {
+		Children = {}
+	}
+	for _, v in next, {...} do table.insert(data.Children, v) end
+	return data
 end
 
-local function MakePinInfo(name : string, adornee: Instance | Vector3 | nil, PinData : PinData | nil, misc: {any} | nil) : PinInfo
-    local info = {
-        Name = name,
-        Adornee = adornee or Vector3.zero,
-        PinData = PinData or MakePinData(),
-    }
-    for i, v in pairs(misc) do if not info[i] then info[i] = v end end
-    return info
+local function MakePinInfo(name : string, adornee: Instance | Vector3 | nil, PinData : PinData | nil, misc: {any}) : PinInfo
+	local info = {
+		Name = name,
+		Adornee = adornee or Vector3.zero,
+		PinData = PinData or MakePinData(),
+	}
+	for i, v in pairs(misc) do if not info[i] then info[i] = v end end
+	return info
 end
 
-function pin.new(name: string, adornee : Instance | Vector3 | nil)
-    ---- Private
-    local Pin = {}
-    local isRunning = false
-    ---- Public
-    Pin.pin = MakePinInfo(name, adornee, nil, {})
-
-    function Pin:AddChild(element: Instance)
-        table.insert(Pin.pin.Children, element)
-    end
-    
-    function Pin:UpdateChildren(callback)
-        for _, child in next, Pin.pin.Children do
-            callback(child)
-        end
-    end
-    
-    function Pin:Enable()
-        if isRunning then return end
-        isRunning = true
-        while task.wait() do 
-            if not isRunning then break end
-            local NextPos = UpdatePinPosition(Pin.pin)
-            Pin:UpdateChildren(function(child)
-                child["Position"] = UDim2.fromOffset(NextPos.X, NextPos.Y)
-            end)
-        end
-    end
-    
-    function Pin:Disable()
-        if not isRunning then return end
-        isRunning = false
-    end
-
-    return Pin
+local function Init()
+	if not pin.Init then
+		pin.DebugRoot = pin.Make "ScreenGui" {
+			Name = "DebuggingV2",
+			Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui"),
+			ResetOnSpawn = false
+		}
+		pin.Init = true
+	end
 end
 
+---- Public Functions
+
+function pin.new(adornee : Instance | Vector3 | nil)
+	---- Private
+	local Pin = {}
+	local isRunning = false
+	local ChildChanged = Instance.new("BindableEvent")
+	local Frame, _ = pin.Make "Frame" {
+		Name = "Debugging",
+		AnchorPoint = Vector2.new(0.5,0.5),
+		Size = UDim2.fromScale(0.1, 0.1),
+		AutomaticSize = Enum.AutomaticSize.X,
+		BackgroundTransparency = 1,
+		[pin.Make "UIListLayout"] = {
+			Name = "ListLayout",
+			SortOrder = Enum.SortOrder.LayoutOrder,
+			VerticalAlignment = Enum.VerticalAlignment.Top,
+			HorizontalAlignment = Enum.HorizontalAlignment.Left
+		},
+		Parent = pin.DebugRoot
+	}
+	---- Public
+	Pin.pin = MakePinInfo(name, adornee, nil, {})
+
+	function Pin:AddChild(element: Instance, properties)
+		ChildChanged:Fire(true, element)
+		table.insert(Pin.pin.PinData.Children, element)
+		return element, properties
+	end
+	
+	function Pin:RemoveChild(element: Instance)
+		ChildChanged:Fire(false, element)
+		table.remove(Pin.pin.PinData.Children, table.find(Pin.pin.PinData.Children, element))
+	end
+
+	function Pin:Enable()
+		if isRunning then return end
+		isRunning = true
+		task.spawn(function()
+			while task.wait() do 
+				if not isRunning then print("Stopped"); break end
+				local NextPos = UpdatePinPosition(Pin.pin)
+				Frame.Position = UDim2.fromOffset(NextPos.X, NextPos.Y)
+			end
+		end)
+	end
+	
+	function Pin:Disable()
+		if not isRunning then return end
+		isRunning = false
+	end
+	
+	local ChangedConnection = ChildChanged.Event:Connect(function(added, element)
+		if not added then
+			element:Destroy()
+			return
+		end
+		element.Parent = Frame
+	end)
+	
+	function Pin:Destroy()
+		ChangedConnection:Disconnect()
+		Frame:Destroy()
+		ChildChanged:Destroy()
+		isRunning = false
+		Pin.pin = nil
+		for i, v in pairs(Pin) do
+			Pin[i] = nil
+		end
+	end
+	
+	return Pin
+end
+
+Init()
 return pin
